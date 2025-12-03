6b934c06285edd7ee392af54652f7abcfabefb05
s3cr3t <baongdqu@gmail.com>
2025-12-03 20:41:36 +0700

    feat: thêm trường thông tin ngày sinh và giới tính của 1 user.

--- app/src/main/java/com/example/app_movie_booking_ticket/extra_user.java
+++ app/src/main/java/com/example/app_movie_booking_ticket/extra_user.java
@@ -5,6 +5,8 @@ public class extra_user {
     public String fullName;
     public String email;
     public String phone;
+    public String dateOfBirth;
+    public String gender;
 
     private String avatarUrl;
 
@@ -13,13 +15,20 @@ public class extra_user {
     }
 
     // ✅ Constructor đầy đủ tham số, gán dữ liệu đúng cách
-    public extra_user(String uid, String fullName, String email, String phone) {
+    public extra_user(String uid, String fullName, String email, String phone, String dateOfBirth, String gender) {
         this.uid = uid;
         this.fullName = fullName;
         this.email = email;
         this.phone = phone;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
     }
 
-    public String getAvatarUrl() { return avatarUrl; }
-    public void setAvatarUrl(String avatarUrl) { this.avatarUrl = avatarUrl; }
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
 }
